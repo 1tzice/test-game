805fbb767e4004e9101972f474bfc655107e6f32
1tzice <griyanarto@gmail.com>
2023-08-05 22:05:35 +0700

    game.php
    tadi salah naro file 😁

--- game.php
+++ game.php
@@ -1,8 +1,15 @@
 <?php
 echo "Welcome to my game/n";
+while (true) {
 echo "===== game tebak angka =====";
 echo "tebak sebuah angka antera 1 dan 9!/n";
 $computer = rand(1,9);
 echo "masukan tebakanmu: ";
 $player = trim(fgets(STDIN));
-echo "$player";
+if($player == $computer){
+    echo "menang. angka komputer adalah $computer. \n";
+    exit;
+}else{
+    echo "kalah. angka komputer adalah $computer. \n";
+}
+}
